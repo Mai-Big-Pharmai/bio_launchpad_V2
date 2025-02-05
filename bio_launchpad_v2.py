61e5c94a7fd82d649b25e8da078feed7ef774b0d
kreios <kreios@bigpharm.ai>
2025-02-05 17:22:47 -0500

    shorter

--- bio_launchpad_v2.py
+++ bio_launchpad_v2.py
@@ -95,7 +95,7 @@ st.markdown(light_theme_css, unsafe_allow_html=True)
 
 # Title and Introduction and Logo image
 image = Image.open("assets/big-pharmai-logo.png")
-resized_image = image.resize((800,200))
+resized_image = image.resize((800,150))
 st.image(resized_image)
 st.title("BADDIE Agent Resource Hub")
 st.markdown("### The De Facto Agent Framework for Biology")
